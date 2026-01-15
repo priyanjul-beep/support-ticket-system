ee3c1faedba5a65395ceda443c405d39a42d2cc3
Priyanjul Sonkar <priyanjul@appsforbharat.com>
2026-01-16 00:48:53 +0530

    Update API and socket URLs for production

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,14 @@
+// import { io } from "socket.io-client";
+
+// const socket = io("http://localhost:5000");
+
+// export default socket;
+
+
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io(import.meta.env.VITE_SOCKET_URL, {
+  transports: ["websocket"]
+});
 
 export default socket;
